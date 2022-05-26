fa850e0313f134987032ffd4b5da554af927594e
Viet Duc Kieu <viet.duc.kieu@web.de>
2022-05-26 17:27:04 +0200

    add template components

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -1,6 +1,10 @@
 import Head from 'next/head'
-import Button from '@mui/material/Button';
-
+import Chart from '../components/Chart';
+import Deposits from '../components/Deposits';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import OrdersTable from '../components/ordersTable';
 
 export default function Home() {
   return (
@@ -10,8 +14,42 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <p>Hello World!</p>
-      <Button variant="contained">Text</Button>
+      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <Grid container spacing={3}>
+          {/* Chart */}
+          <Grid item xs={12} md={8} lg={9}>
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                height: 240,
+              }}
+            >
+              <Chart />
+            </Paper>
+          </Grid>
+          {/* Recent Deposits */}
+          <Grid item xs={12} md={4} lg={3}>
+            <Paper
+              sx={{
+                p: 2,
+                display: 'flex',
+                flexDirection: 'column',
+                height: 240,
+              }}
+            >
+              <Deposits />
+            </Paper>
+          </Grid>
+          {/* Recent Orders */}
+          <Grid item xs={12}>
+            <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+              <OrdersTable />
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
 
     </div>
   )
