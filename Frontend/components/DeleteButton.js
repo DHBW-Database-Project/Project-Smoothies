e9e67124a54e9bfefc34e2ac61c989f360a47372
Viet Duc Kieu <viet.duc.kieu@web.de>
2022-05-28 22:02:06 +0200

    implement delete front end side for supplier

--- Frontend/components/DeleteButton.js
+++ Frontend/components/DeleteButton.js
@@ -3,8 +3,22 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 
+
+
 const DeleteButton = ({ tableName, rowId }) => {
+    const options = {
+        method: "DELETE",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json;charset=UTF-8",
+        },
+        body: JSON.stringify({
+            rowId: rowId
+        })
+    }
+
     const handleDelete = () => {
+        fetch(process.env.SUPPLIER_URL, options)
         console.log(tableName, rowId)
     }
 
