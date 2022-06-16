0e66b873a60657a1801a09d8254fd302e986dd66
Anh Vu <89083753+anhvu202@users.noreply.github.com>
2022-06-16 20:35:41 +0200

    remove deposit in homepage

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -15,31 +15,6 @@ export default function Home() {
 
       <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
         <Grid container spacing={3}>
-          {/* Chart */}
-          {/* <Grid item xs={12} md={8} lg={9}>
-            <Paper
-              sx={{
-                p: 2,
-                display: 'flex',
-                flexDirection: 'column',
-                height: 240,
-              }}
-            >
-            </Paper>
-          </Grid> */}
-          {/* Recent Deposits */}
-          <Grid item xs={12} md={4} lg={3}>
-            <Paper
-              sx={{
-                p: 2,
-                display: 'flex',
-                flexDirection: 'column',
-                height: 240,
-              }}
-            >
-              {/* <Deposits /> */}
-            </Paper>
-          </Grid>
           {/* Recent Orders */}
           <Grid item xs={12}>
             <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
